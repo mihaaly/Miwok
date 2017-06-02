d05ad651391456f8ca67f17efb46bd03bf3e9c60
mihaaly <mihaaly@gmail.com>
2017-06-02 16:02:26 +0200

    adds ViewPager and FragmentPagerAdapter

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -15,88 +15,26 @@
  */
 package com.example.android.miwok;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
 
-        // Find the view that shows the numbers category
-        TextView numbers = (TextView) findViewById(R.id.numbers);
+        // Find the view pager that will allow the user to swipe between fragments
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
 
-        // Set a click listener on that view
-        numbers.setOnClickListener(new View.OnClickListener() {
-            // The code in this method will be executed when numbers category is clicked on
-            @Override
-            public void onClick(View v) {
-                // Create a new intent to open the {@link NumbersActivity}
-                Intent numbersIntent = new Intent(MainActivity.this, NumbersActivity.class);
+        // Create an adapter that knows which fragment should be shown on each page
+        SimpleFragmentPagerAdapter adapter = new SimpleFragmentPagerAdapter(getSupportFragmentManager());
 
-                // Start the new activity
-                startActivity(numbersIntent);
-            }
-        });
-
-        // Find the view that shows family  category
-        TextView family = (TextView) findViewById(R.id.family);
-
-        // Set click listener on that view
-        family.setOnClickListener(new View.OnClickListener() {
-            // The code in this method will be executed when family category is clicked on
-            @Override
-            public void onClick(View v) {
-                // Create a new intent to open the {@link FamilyActivity}
-                Intent familyIntent = new Intent(MainActivity.this, FamilyActivity.class);
-
-                // Start the new activity
-                startActivity(familyIntent);
-            }
-        });
-
-        // Find the view that shows colors category
-        TextView colors = (TextView) findViewById(R.id.colors);
-
-        // Set click listener on that view
-        colors.setOnClickListener(new View.OnClickListener() {
-            // The code in this method will be executed when colors category is clicked on
-            @Override
-            public void onClick(View v) {
-                // Create a new intent to open the {@link ColorsActivity}
-                Intent colorsIntent = new Intent(MainActivity.this, ColorsActivity.class);
-
-                // Start the new activity
-                startActivity(colorsIntent);
-            }
-        });
-
-        // Find the view that shows phrases category
-        TextView phrases = (TextView) findViewById(R.id.phrases);
-
-        // Set click listener on that view
-        phrases.setOnClickListener(new View.OnClickListener() {
-            // The code in this method will be executed when phrases category is clicked on
-            @Override
-            public void onClick(View v) {
-                // Create a new intent to open the {@link PhrasesActivity}
-                Intent phrasesIntent = new Intent(MainActivity.this, PhrasesActivity.class);
-
-                // Start the new activity
-                startActivity(phrasesIntent);
-            }
-        });
-    }
-
-    public void toTestView(View v) {
-        Intent goToTestView = new Intent(MainActivity.this, TestActivity.class);
-        startActivity(goToTestView);
+        // Set the adapter onto the view pager
+        viewPager.setAdapter(adapter);
     }
 }
+
